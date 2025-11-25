cc73920001b1f95c8b98b613779b33c3ac81ab67
AslamE1 <eg244942@engug.ruh.ac.lk>
2025-11-25 21:57:49 +0530

    modified

---
+++ README.md
@@ -0,0 +1,207 @@
+# 📋 Celebrity Database Master List (176 Total)
+
+## 🎬 Bollywood Stars (99)
+
+### Men (50)
+* Aamir Khan
+* Abhay Deol
+* Abhishek Bachchan
+* Aftab Shivdasani
+* Ajay Devgn
+* Akshay Kumar
+* Akshaye Khanna
+* Amitabh Bachchan
+* Anil Kapoor
+* Arjun Kapoor
+* Arjun Rampal
+* Arshad Warsi
+* Ayushmann Khurrana
+* Bobby Deol
+* Emraan Hashmi
+* Farhan Akhtar
+* Govinda
+* Hrithik Roshan
+* Irrfan Khan
+* John Abraham
+* Kartik Aaryan
+* Kunal Khemu
+* Manoj Bajpayee
+* Nana Patekar
+* Naseeruddin Shah
+* Paresh Rawal
+* Prabhas
+* R Madhavan
+* Rajkummar Rao
+* Ranbir Kapoor
+* Randeep Hooda
+* Ranveer Singh
+* Riteish Deshmukh
+* Saif Ali Khan
+* Salman Khan
+* Sanjay Dutt
+* Shah Rukh Khan
+* Shahid Kapoor
+* Shreyas Talpade
+* Sidharth Malhotra
+* Suniel Shetty
+* Sunny Deol
+* Sushant Singh Rajput
+* Tiger Shroff
+* Tusshar Kapoor
+* Uday Chopra
+* Varun Dhawan
+* Vicky Kaushal
+* Vivek Oberoi
+
+### Women (49)
+* Aishwarya Rai
+* Alia Bhatt
+* Ameesha Patel
+* Amrita Rao
+* Amy Jackson
+* Anushka Sharma
+* Anushka Shetty
+* Asin
+* Bhumi Pednekar
+* Bipasha Basu
+* Deepika Padukone
+* Disha Patani
+* Esha Gupta
+* Huma Qureshi
+* Jacqueline Fernandez
+* Juhi Chawla
+* Kajal Aggarwal
+* Kajol
+* Kangana Ranaut
+* Kareena Kapoor
+* Karisma Kapoor
+* Katrina Kaif
+* Kiara Advani
+* Kriti Kharbanda
+* Kriti Sanon
+* Lara Dutta
+* Madhuri Dixit
+* Mrunal Thakur
+* Nargis Fakhri
+* Nushrat Bharucha
+* Parineeti Chopra
+* Pooja Hegde
+* Prachi Desai
+* Preity Zinta
+* Priyanka Chopra
+* Rani Mukerji
+* Richa Chadda
+* Sara Ali Khan
+* Shilpa Shetty
+* Shraddha Kapoor
+* Shruti Haasan
+* Sonakshi Sinha
+* Sonam Kapoor
+* Taapsee Pannu
+* Tabu
+* Tamannaah Bhatia
+* Vaani Kapoor
+* Vidya Balan
+* Yami Gautam
+* Zareen Khan
+
+---
+
+## Hollywood Stars (17)
+* Angelina Jolie
+* Brad Pitt
+* Denzel Washington
+* Hugh Jackman
+* Jennifer Lawrence
+* Johnny Depp
+* Kate Winslet
+* Leonardo DiCaprio
+* Megan Fox
+* Natalie Portman
+* Nicole Kidman
+* Robert Downey Jr.
+* Sandra Bullock
+* Scarlett Johansson
+* Tom Cruise
+* Tom Hanks
+* Will Smith
+
+---
+
+## Sri Lankan Cricketers (36)
+
+### Men (29)
+* Aravinda de Silva
+* Arjuna Ranatunga
+* Chaminda Vaas
+* Charith Asalanka
+* Dhananjaya de Silva
+* Dimuth Karunaratne
+* Dinesh Chandimal
+* Dushmantha Chameera
+* Kumar Sangakkara
+* Kusal Mendis
+* Kusal Perera
+* Lahiru Thirimanne
+* Lasith Malinga
+* Maheesh Theekshana
+* Mahela Jayawardene
+* Marvan Atapattu
+* Matheesha Pathirana
+* Muttiah Muralitharan
+* Niroshan Dickwella
+* Nuwan Kulasekara
+* Pathum Nissanka
+* Pramod Madushan
+* Rangana Herath
+* Romesh Kaluwitharana
+* Sanath Jayasuriya
+* Suranga Lakmal
+* Thisara Perera
+* Tillakaratne Dilshan
+* Upul Tharanga
+
+### Women (7)
+* Anushka Sanjeewani
+* Chamari Athapaththu
+* Kavisha Dilhari
+* Nilakshi de Silva
+* Oshadi Ranasinghe
+* Shashikala Siriwardene
+* Sugandika Kumari
+
+---
+
+## South Asian Cricketers (17)
+
+### Men (10)
+* Babar Azam
+* Imran Khan
+* Mashrafe Mortaza
+* Rohit Sharma
+* Shahid Afridi
+* Shakib Al Hasan
+* Shoaib Akhtar
+* Tamim Iqbal
+* Wasim Akram
+* Yuvraj Singh
+
+### Women (7)
+* Bismah Maroof
+* Harmanpreet Kaur
+* Jahanara Alam
+* Jemimah Rodrigues
+* Kainat Imtiaz
+* Nida Dar
+* Smriti Mandhana
+
+---
+
+## Global Sports Icons (7)
+* Cristiano Ronaldo
+* Kobe Bryant
+* Maria Sharapova
+* Mithali Raj
+* MS Dhoni
+* Sachin Tendulkar
+* Virat Kohli
--- encode_pht.py
+++ encode_pht.py
@@ -2,16 +2,69 @@ import os
 import pickle
 import numpy as np 
 from deepface import DeepFace
-
-
 male_list = [
-    "virat_kohli"
+    # Bollywood (50)
+    "Aamir_Khan", "Abhay_Deol", "Abhishek_Bachchan", "Aftab_Shivdasani", "Ajay_Devgn",
+    "Akshay_Kumar", "Akshaye_Khanna", "Amitabh_Bachchan", "Anil_Kapoor", "Arjun_Kapoor",
+    "Arjun_Rampal", "Arshad_Warsi", "Ayushmann_Khurrana", "Bobby_Deol", "Emraan_Hashmi",
+    "Farhan_Akhtar", "Govinda", "Hrithik_Roshan", "Irrfan_Khan", "John_Abraham",
+    "Kartik_Aaryan", "Kunal_Khemu", "Manoj_Bajpayee", "Nana_Patekar", "Naseeruddin_Shah",
+    "Paresh_Rawal", "Prabhas", "R_Madhavan", "Rajkummar_Rao", "Ranbir_Kapoor",
+    "Randeep_Hooda", "Ranveer_Singh", "Riteish_Deshmukh", "Saif_Ali_Khan", "Salman_Khan",
+    "Sanjay_Dutt", "Shah_Rukh_Khan", "Shahid_Kapoor", "Shreyas_Talpade", "Sidharth_Malhotra",
+    "Suniel_Shetty", "Sunny_Deol", "Sushant_Singh_Rajput", 
+    "Tiger_Shroff", "Tusshar_Kapoor", "Uday_Chopra", "Varun_Dhawan", "Vicky_Kaushal", "Vivek_Oberoi",
+    
+    # Hollywood (9)
+    "Brad Pitt", "Denzel Washington", "Hugh Jackman", "Johnny Depp", "Leonardo DiCaprio",
+    "Robert Downey Jr.", "Tom Cruise", "Tom Hanks", "Will Smith",
+    
+    # Global Sports (5)
+    # Removed: Harry Kane
+    "Cristiano Ronaldo", "Kobe Bryant", "ms_dhoni", "sachin_tendulkar", "virat_kohli",
+
+    # Sri Lankan Cricketers (29)
+    "Aravinda de Silva", "Arjuna Ranatunga", "Chaminda Vaas", "Charith Asalanka", 
+    "Dhananjaya de Silva", "Dimuth Karunaratne", "Dinesh Chandimal", "Dushmantha Chameera",
+    "Kumar Sangakkara", "Kusal Mendis", "Kusal Perera", "Lahiru Thirimanne", "Lasith Malinga",
+    "Maheesh Theekshana", "Mahela Jayawardene", "Marvan Atapattu", "Matheesha Pathirana",
+    "Muttiah Muralitharan", "Niroshan Dickwella", "Nuwan Kulasekara", "Pathum Nissanka",
+    "Pramod Madushan", "Rangana Herath", "Romesh Kaluwitharana", "Sanath Jayasuriya",
+    "Suranga Lakmal", "Thisara Perera", "Tillakaratne Dilshan", "Upul Tharanga",
+
+    # South Asian Cricketers (10)
+    "Babar Azam", "Imran Khan", "Mashrafe Mortaza", "Rohit Sharma", "Shahid Afridi",
+    "Shakib Al Hasan", "Shoaib Akhtar", "Tamim Iqbal", "Wasim Akram", "Yuvraj Singh"
 ]
 
 female_list = [
-    "Aishwarya_Rai"
-]
+    # Bollywood (49)
+    "Aishwarya_Rai", "Alia_Bhatt", "Ameesha_Patel", "Amrita_Rao", "Amy_Jackson",
+    "Anushka_Sharma", "Anushka_Shetty", "Asin", "Bhumi_Pednekar", "Bipasha_Basu",
+    "Deepika_Padukone", "Disha_Patani", "Esha_Gupta", "Huma_Qureshi", 
+    "Jacqueline_Fernandez", "Juhi_Chawla", "Kajal_Aggarwal", "Kajol", "Kangana_Ranaut",
+    "Kareena_Kapoor", "Karisma_Kapoor", "Katrina_Kaif", "Kiara_Advani", "Kriti_Kharbanda",
+    "Kriti_Sanon", "Lara_Dutta", "Madhuri_Dixit", "Mrunal_Thakur", "Nargis_Fakhri",
+    "Nushrat_Bharucha", "Parineeti_Chopra", "Pooja_Hegde", "Prachi_Desai", "Preity_Zinta",
+    "Priyanka_Chopra", "Rani_Mukerji", "Richa_Chadda", "Sara_Ali_Khan", "Shilpa_Shetty",
+    "Shraddha_Kapoor", "Shruti_Haasan", "Sonakshi_Sinha", "Sonam_Kapoor", "Taapsee_Pannu",
+    "Tabu", "Tamannaah_Bhatia", "Vaani_Kapoor", "Vidya_Balan", "Yami_Gautam", "Zareen_Khan",
+    
+    # Hollywood (8)
+    "Angelina Jolie", "Jennifer Lawrence", "Kate Winslet", "Megan Fox", "Natalie Portman",
+    "Nicole Kidman", "Sandra Bullock", "Scarlett Johansson",
+    
+    # Global Sports (2)
+    "Maria Sharapova", "mithali_raj",
 
+    # Sri Lankan Cricketers (7)
+    "Anushka Sanjeewani", "Chamari Athapaththu", "Kavisha Dilhari", "Nilakshi de Silva",
+    "Oshadi Ranasinghe", "Shashikala Siriwardene", "Sugandika Kumari",
+
+    # South Asian Cricketers (7)
+    "Bismah Maroof", "Harmanpreet Kaur", "Jahanara Alam", "Jemimah Rodrigues",
+    "Kainat Imtiaz", "Nida Dar", "Smriti Mandhana"
+]
 print(f"[INFO] Starting to encode all {len(male_list) + len(female_list)} celebrities...")
 
 
@@ -53,7 +106,7 @@ for celebrity_dir in album_dirs:
        
         person_embeddings = []
         
-        # Now we loop through all their photos
+       
         for image_name in os.listdir(person_dir):
             image_path = os.path.join(person_dir, image_name)
             
@@ -61,28 +114,27 @@ for celebrity_dir in album_dirs:
                 continue
                 
             try:
-                # Get the "fingerprint" for this single photo
+               
                 embedding_obj = DeepFace.represent(
                     img_path=image_path, 
                     model_name=model_name,
                     enforce_detection=True
                 )
-                # Add the fingerprint to our person's list
+                
                 person_embeddings.append(embedding_obj[0]["embedding"])
                 
             except Exception as e:
-                # This just skips a blurry/bad photo
+                
                 print(f"    [SKIP] Could not process '{image_path}': {e}")
 
       
         if len(person_embeddings) > 0:
-            # This is the magic line.
-            # np.mean averages all the lists together into one "master" list.
+            
             master_embedding = np.mean(person_embeddings, axis=0)
             
-            # Save the ONE master fingerprint
+            
             all_master_encodings.append({
-                "name": check_name_clean, # Save the clean name
+                "name": check_name_clean, 
                 "embedding": master_embedding,
                 "gender": gender
             })
@@ -90,9 +142,7 @@ for celebrity_dir in album_dirs:
         else:
             print(f"    [FAIL] No valid photos found for {person_name}, skipping.")
             
-# --- END OF NEW LOGIC ---
 
-# --- Save to your final "cheat sheet" file ---
 output_file = "deepface_encodings_PROTOTYPE.pickle"
 print(f"\n[INFO] Serializing {len(all_master_encodings)} total master encodings...")
 with open(output_file, "wb") as f:
